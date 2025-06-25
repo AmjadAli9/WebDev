2da1d086605b740174c4b120eb7c3ce6e44a064f
Mohd Amjad Ali <2003amjadali@gmail.com>
2025-06-26 00:53:52 +0530

    🔥 Fixed merge conflict in index.js

--- src/index.js
+++ src/index.js
@@ -6,11 +6,10 @@ import { BrowserRouter } from "react-router-dom"; // ✅ Only here
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-<<<<<<< HEAD
-    <BrowserRouter> 
-=======
-    <BrowserRouter> {/* ✅ Only ONE Router here */}
->>>>>>> 9b2d469fd6f28892b7277d47845433741d5b49ba
+
+
+    <BrowserRouter>
+
       <App />
     </BrowserRouter>
   </React.StrictMode>
